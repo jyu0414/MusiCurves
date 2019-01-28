4c8f84652f84d9cec2cdb04d2770b565f9012dba
Yuji Sasaki <jyu0414@gmail.com>
2019-01-28 21:10:47 +0900

    倍音

---
+++ src/jp/ac/keio/sfc/oop/musicurves/Harmonic.java
@@ -0,0 +1,11 @@
+package jp.ac.keio.sfc.oop.musicurves;
+
+public class Harmonic {
+    double multiple = 1.0;
+    double amplitude = 1.0;
+    Harmonic(double _multiple, double _amplitude)
+    {
+        multiple = _multiple;
+        amplitude = _amplitude;
+    }
+}
--- src/jp/ac/keio/sfc/oop/musicurves/Melody.java
+++ src/jp/ac/keio/sfc/oop/musicurves/Melody.java
@@ -8,16 +8,37 @@ public class Melody {
     private static final float SAMPLE_RATE = 44100;
     private static final double VOLUME = 0.9;
     private ArrayList<Byte> data = new ArrayList<>();
+    private ArrayList<Harmonic> harmonics = new ArrayList<>();
+
+    Melody(ArrayList<Harmonic> _harmonics){
+        harmonics = _harmonics;
+    }
+
+    Melody(){
+        harmonics.add(new Harmonic(1,1));
+        harmonics.add(new Harmonic(2,1.0/3.0));
+        harmonics.add(new Harmonic(3,1.0/5.0));
+        harmonics.add(new Harmonic(4,1.0/7.0));
+        harmonics.add(new Harmonic(5,1.0/9.0));
+    }
 
     void addPitch(double frequency, double sec) {
+        for(int i = 0; i < sec * SAMPLE_RATE; i++) {
+            data.add(additiveSynthesis(frequency));
+        }
+    }
 
-        double angularVelocity = 2 * Math.PI / SAMPLE_RATE * frequency; //角速度
-        for(int i = 0; i < sec * SAMPLE_RATE; i++)
-        {
-            data.add((byte) (128 * VOLUME * Math.sin(data.size() * angularVelocity)));
+    private byte additiveSynthesis(double frequency)
+    {
+        double result = 0;
+
+        for(int i = 0; i < harmonics.size(); i++) {
+            double angularVelocity = 2 * Math.PI / SAMPLE_RATE * frequency * harmonics.get(i).multiple; //角速度
+            result += (128 * VOLUME * harmonics.get(i).amplitude * Math.sin(data.size() * angularVelocity));
         }
 
 
+        return (byte)result;
     }
 
     AudioFormat getFormat(){
